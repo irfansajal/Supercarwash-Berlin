7153b0680f8ecde13d642b3310034639aa644015
irfansajal <irfan@irfans-mbp.speedport.ip>
2024-03-22 15:35:17 +0100

    second

---
+++ signupforabo.js
@@ -0,0 +1,9 @@
+function submission(){
+    Swal.fire({
+        title: 'Hello!',
+        text: 'Your application submitted successfully',
+        icon: 'success', // 'success', 'error', 'warning', 'info', 'question'
+        confirmButtonText: 'ok'
+      });
+      
+}
